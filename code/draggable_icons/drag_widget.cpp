66bf5460d6ead423e2eeedbfc5a2a576ddbc53fa
tonghao.yuan <tonghao.yuan@csdental.com>
2020-06-22 15:34:58 +0800

    feat: avoid conflicts between click and drag

--- code/draggable_icons/drag_widget.cpp
+++ code/draggable_icons/drag_widget.cpp
@@ -1,4 +1,7 @@
 #include "drag_widget.h"
+#include "qframe.h"
+#include "qgesture.h"
+#include "qnamespace.h"
 
 #include <QDrag>
 #include <QDragEnterEvent>
@@ -10,7 +13,7 @@
 
 DraggableWidget::DraggableWidget(QWidget* parent) : QFrame(parent) {
   setMinimumSize(200, 200);
-  setFrameStyle(QFrame::Sunken | QFrame::StyledPanel);
+  // setFrameStyle(QFrame::Sunken | QFrame::StyledPanel);
   setAcceptDrops(true);
 
   auto* boat_icon = new QLabel(this);
@@ -30,6 +33,8 @@ DraggableWidget::DraggableWidget(QWidget* parent) : QFrame(parent) {
   house_icon->move(10, 80);
   house_icon->show();
   house_icon->setAttribute(Qt::WA_DeleteOnClose);
+
+  grabGesture(Qt::TapAndHoldGesture);
 }
 
 void DraggableWidget::dragEnterEvent(QDragEnterEvent* event) {
@@ -58,34 +63,86 @@ void DraggableWidget::dragMoveEvent(QDragMoveEvent* event) {
   }
 }
 
-void DraggableWidget::dropEvent(QDropEvent* event) {
-  if (event->mimeData()->hasFormat("application/x-dnditemdata")) {
-    QByteArray itemData = event->mimeData()->data("application/x-dnditemdata");
-    QDataStream dataStream(&itemData, QIODevice::ReadOnly);
+// void DraggableWidget::dropEvent(QDropEvent* event) {
+//   if (event->mimeData()->hasFormat("application/x-dnditemdata")) {
+//     QByteArray itemData = event->mimeData()->data("application/x-dnditemdata");
+//     QDataStream dataStream(&itemData, QIODevice::ReadOnly);
 
-    QPixmap pixmap;
-    QPoint offset;
-    dataStream >> pixmap >> offset;
+//     QPixmap pixmap;
+//     QPoint offset;
+//     dataStream >> pixmap >> offset;
 
-    auto* newIcon = new QLabel(this);
-    newIcon->setPixmap(pixmap);
-    newIcon->move(event->pos() - offset);
-    newIcon->show();
-    newIcon->setAttribute(Qt::WA_DeleteOnClose);
+//     auto* newIcon = new QLabel(this);
+//     newIcon->setPixmap(pixmap);
+//     newIcon->move(event->pos() - offset);
+//     newIcon->show();
+//     newIcon->setAttribute(Qt::WA_DeleteOnClose);
 
-    if (event->source() == this) {
-      event->setDropAction(Qt::MoveAction);
-      event->accept();
-    } else {
-      event->acceptProposedAction();
-    }
-  } else {
-    event->ignore();
+//     if (event->source() == this) {
+//       event->setDropAction(Qt::MoveAction);
+//       event->accept();
+//     } else {
+//       event->acceptProposedAction();
+//     }
+//   } else {
+//     event->ignore();
+//   }
+// }
+
+// void DraggableWidget::mousePressEvent(QMouseEvent* event) {
+//   // return QFrame::mousePressEvent(event);
+
+//   QLabel* child = static_cast<QLabel*>(childAt(event->pos()));
+//   if (child == nullptr)
+//     return;
+
+//   QPixmap pixmap = *child->pixmap();
+
+//   QByteArray itemData;
+//   QDataStream dataStream(&itemData, QIODevice::WriteOnly);
+//   dataStream << pixmap << QPoint(event->pos() - child->pos());
+
+//   auto* mimeData = new QMimeData;
+//   mimeData->setData("application/x-dnditemdata", itemData);
+
+//   auto* drag = new QDrag(this);
+//   drag->setMimeData(mimeData);
+//   drag->setPixmap(pixmap);
+//   drag->setHotSpot(event->pos() - child->pos());
+
+//   if (drag->exec(Qt::CopyAction | Qt::MoveAction, Qt::CopyAction) == Qt::MoveAction) {
+//     child->close();
+//   } else {
+//     child->show();
+//     child->setPixmap(pixmap);
+//   }
+// }
+
+bool DraggableWidget::event(QEvent* event) {
+  if (event->type() == QEvent::Gesture) {
+    return gesture_event(dynamic_cast<QGestureEvent*>(event));
   }
+
+  return QFrame::event(event);
 }
 
-void DraggableWidget::mousePressEvent(QMouseEvent* event) {
-  QLabel* child = static_cast<QLabel*>(childAt(event->pos()));
+bool DraggableWidget::gesture_event(QGestureEvent* event) {
+  if (event->gesture(Qt::TapAndHoldGesture)) {
+    on_tap_and_hold(event);
+  }
+  return true;
+}
+
+void DraggableWidget::on_tap_and_hold(QGestureEvent* event) {
+  auto gesture = dynamic_cast<QTapAndHoldGesture*>(event->gesture(Qt::TapAndHoldGesture));
+  if (!gesture) {
+    // TODO tonghao log here
+    //    LOG_W("can not create gesture from gesture event");
+    return;
+  }
+
+  const auto pos = mapFromGlobal(gesture->position().toPoint());
+  QLabel* child = static_cast<QLabel*>(childAt(pos));
   if (child == nullptr)
     return;
 
@@ -93,7 +150,7 @@ void DraggableWidget::mousePressEvent(QMouseEvent* event) {
 
   QByteArray itemData;
   QDataStream dataStream(&itemData, QIODevice::WriteOnly);
-  dataStream << pixmap << QPoint(event->pos() - child->pos());
+  dataStream << pixmap << QPoint(pos - child->pos());
 
   auto* mimeData = new QMimeData;
   mimeData->setData("application/x-dnditemdata", itemData);
@@ -101,15 +158,7 @@ void DraggableWidget::mousePressEvent(QMouseEvent* event) {
   auto* drag = new QDrag(this);
   drag->setMimeData(mimeData);
   drag->setPixmap(pixmap);
-  drag->setHotSpot(event->pos() - child->pos());
-
-  QPixmap& tempPixmap = pixmap;
-  QPainter painter;
-  painter.begin(&tempPixmap);
-  painter.fillRect(pixmap.rect(), QColor(127, 127, 127, 127));
-  painter.end();
-
-  child->setPixmap(tempPixmap);
+  drag->setHotSpot(pos - child->pos());
 
   if (drag->exec(Qt::CopyAction | Qt::MoveAction, Qt::CopyAction) == Qt::MoveAction) {
     child->close();
--- code/draggable_icons/drag_widget.h
+++ code/draggable_icons/drag_widget.h
@@ -12,6 +12,7 @@
  */
 
 #include <QFrame>
+#include <QGestureEvent>
 
 class DraggableWidget : public QFrame {
   Q_OBJECT
@@ -21,6 +22,11 @@ public:
 protected:
   void dragEnterEvent(QDragEnterEvent* event) override;
   void dragMoveEvent(QDragMoveEvent* event) override;
-  void dropEvent(QDropEvent* event) override;
-  void mousePressEvent(QMouseEvent* event) override;
+  // void dropEvent(QDropEvent* event) override;
+  // void mousePressEvent(QMouseEvent* event) override;
+  bool event(QEvent* event) override;
+
+private:
+  bool gesture_event(QGestureEvent* ev);
+  void on_tap_and_hold(QGestureEvent* event);
 };
